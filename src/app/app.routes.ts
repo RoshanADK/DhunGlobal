429f61e60fdcc457dcef301c6912987216d204ec
RoshanADK <roshannadhikari039@gmail.com>
2024-10-12 18:28:26 +0545

    Init Commit Ever

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'hello-world',
+  loadComponent:() => import('./hello-world/hello-world.component').then((m) => m.HelloWorldComponent),
+  },
+
+  { path: 'add-song',
+    loadComponent:() => import('./add-song/add-song.component').then((m) => m.AddSongComponent),
+  },
+
+  { path: '',
+    redirectTo: 'hello-world',
+    pathMatch: 'full',
+  },
+];
